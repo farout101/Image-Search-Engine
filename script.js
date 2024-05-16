468e07c3cd358e57917918d9280e11d2d2c3c30b
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-05-17 02:00:49 +0630

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Hello");
+// Access Key - cu3LKjL5__dbJt-cOgaINxfojsaOiWj1abQm-jbTItI
+// API URL - https://api.unsplash.com/search/photos?page=1&query=office
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const searchMoreBtn = document.getElementById("search-more-btn");
+
+const accessKey = "cu3LKjL5__dbJt-cOgaINxfojsaOiWj1abQm-jbTItI";
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+	keyword = searchBox.value;
+	const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+	const response = await fetch(url);
+	const data = await response.json();
+
+	const results = data.results;
+
+	results.map((result) => {
+		const image = document.createElement("img");
+		image.src = result.urls.small;
+
+		const imageLink = document.createElement("a");
+		imageLink.href = result.links.html;
+		imageLink.target = "_blank";
+
+		imageLink.appendChild(image);
+		searchResult.appendChild(imageLink);
+	});
+}
+
+searchForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	page = 1;
+	searchImages();
+});
